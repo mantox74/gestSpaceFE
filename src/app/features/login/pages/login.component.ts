bc15c8aac6606b256bf03252103c692bf276a8fd
Massimiliano Mantovani <m.mantovani@reactive-almaviva.it>
2026-06-23 17:11:38 +0200

    WIP - logn component

--- src/app/features/login/pages/login.component.ts
+++ src/app/features/login/pages/login.component.ts
@@ -1,9 +1,38 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, signal } from '@angular/core';
+import { form, FormField, required } from '@angular/forms/signals';
+import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
+import { InputTextModule } from 'primeng/inputtext';
+import { PasswordModule } from 'primeng/password';
 
+type LoginFormType = {
+  username: string;
+  password: string;
+};
 @Component({
   selector: 'app-login.component',
-  imports: [],
+  imports: [CommonModule, InputTextModule, CardModule, PasswordModule, FormField, ButtonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
 })
-export class LoginComponent {}
+export class LoginComponent {
+  loginForm = form(
+    signal<LoginFormType>({
+      username: '',
+      password: '',
+    }),
+    (schemaPath) => {
+      required(schemaPath.username, { message: `Username è obbligatorio` });
+      required(schemaPath.password, { message: `Password è obbligatorio` });
+    },
+  );
+
+  login(): void {
+    if (this.loginForm().valid()) {
+      console.log('Login con', this.loginForm().value());
+    } else {
+      console.log('Form non valido', this.loginForm().errors());
+    }
+  }
+}
