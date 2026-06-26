96c45035b5696fbc97ecb358ce141ace7c846a91
Massimiliano Mantovani <m.mantovani@reactive-almaviva.it>
2026-06-26 15:43:12 +0200

    sistemata login e aggiunta gestione environment

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,27 +1,30 @@
 import { CommonModule } from '@angular/common';
-import { Component, inject, signal } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
 import { Router, RouterOutlet } from '@angular/router';
 import { AuthService } from '@app/core/auth/auth.service';
 import { SpinnerComponent } from './shared/components/spinner/spinner.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MatButtonModule, CommonModule, SpinnerComponent, MatIconModule],
+  imports: [
+    RouterOutlet,
+    MatButtonModule,
+    CommonModule,
+    SpinnerComponent,
+    MatIconModule,
+    MatSidenavModule,
+  ],
   templateUrl: './app.html',
   styleUrls: ['./app.scss'],
 })
 export class App {
-  isMenuOpen = signal(false);
   readonly authService: AuthService = inject(AuthService);
   private router = inject(Router);
 
   navigateToLogin() {
     this.router.navigate(['/login']);
-  }
-
-  toggleMenu(): void {
-    this.isMenuOpen.update((open) => !open);
   }
 }
